eaf85c319e1c2c381e83cc4e6326012472dae7b5
Lewis Benson <tm.lewis.benson@gmail.com>
2024-05-06 19:08:33 -0400

    update versions

--- tom-roles.js
+++ tom-roles.js
@@ -51,7 +51,7 @@ async function startRandomMessageInterval() {
 }
 
 client.once('ready', async () => {
-  console.log(`Logged in as ${client.user.tag}! v1.1.27`);
+  console.log(`Logged in as ${client.user.tag}! v1.1.28`);
   startRandomMessageInterval();
 
   client.on('messageCreate', async (message) => {
